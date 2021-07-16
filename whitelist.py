bc74105bfb37a260e312019685d232aca61f932b
Maggie Lin <zhang.miao.lin@uwaterloo.ca>
2021-07-16 13:37:51 -0400

    Updated whitelist algorithm to use preprocess on string

--- whitelist.py
+++ whitelist.py
@@ -1,16 +1,11 @@
 from categories import *
 from random import randrange
-
+from preprocess import *
 
 # whitelist: headline, whitelist_dictionary -> category
 #   string, (string, string) -> category enum
-
-# need to replace
-def preprocess(headline):
-    return ["apple", "walk", "take", "python"]
-
 def whitelist(headline, whitelist_dictionary):
-    words = preprocess(headline)
+    words = preprocess(headline, True, True)
 
     for word in words:
         if word in whitelist_dictionary:
