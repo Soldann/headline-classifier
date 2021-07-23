487f91970850a81bfdbd042e81719440ec0ad474
Soldann <guotl321@gmail.com>
2021-07-22 23:28:25 -0400

    add calculator for accuracy and precision

--- predict.py
+++ predict.py
@@ -1,3 +1,4 @@
+from typing import List
 from bayes import *
 from whitelist import *
 import probabilities
@@ -15,3 +16,37 @@ class Predict:
 
     def whitelist(self,headline: str):
         return whitelist(headline,self.whitelist_dictionary,self.filter_stop_words,self.stem_words)
+
+    def __test(self,test_data: List[List[str]], labels: List, prediction_func):
+        headlineIndex = labels.index('Headline')
+        categoryIndex = labels.index('Category')
+
+        TP = 0 # true positives
+        FP = 0 # false positives
+        TN = 0 # true negatives
+        FN = 0 # false negatives
+
+        for row in test_data:
+            test_result = prediction_func(row[headlineIndex])
+            true_category = Category[row[categoryIndex].upper().replace(' ','_')] # convert category names to enum
+
+            for category in Category:
+                if category == test_result and true_category == category:
+                    TP += 1
+                elif category == test_result and true_category != category:
+                    FP += 1
+                elif category != test_result and true_category != category:
+                    TN += 1
+                else:
+                    FN += 1
+        
+        accuracy = (TP + TN)/(TP + FP + TN + FN)
+        precision = TP/(TP + FP)
+
+        return accuracy, precision
+
+    def test_bayes(self,test_data: List[List[str]], labels: List):
+        return self.__test(test_data,labels,self.bayes)
+
+    def test_whitelist(self,test_data: List[List[str]], labels: List):
+        return self.__test(test_data,labels,self.whitelist)
--- run.py
+++ run.py
@@ -1,9 +1,14 @@
+from csv_reader import read_data
 from preprocess import preprocess
 from categories import Category
 from predict import Predict
 
 headline = "Sonic the Hedgehog is in Minecraft now"
 
-predictor = Predict('training.csv',False,True)
+predictor = Predict('training.csv',True,True)
 
-print(predictor.bayes(headline))
+data, labels = read_data('Polygon Article Categorization Dataset.csv')
+
+print(predictor.test_bayes(data,labels))
+
+print(predictor.test_whitelist(data,labels))
