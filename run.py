28f1675223e9f7e672e298180919595ea22cc107
Soldann <guotl321@gmail.com>
2021-07-22 23:29:54 -0400

    clean up run.py

--- run.py
+++ run.py
@@ -3,12 +3,10 @@ from preprocess import preprocess
 from categories import Category
 from predict import Predict
 
-headline = "Sonic the Hedgehog is in Minecraft now"
-
 predictor = Predict('training.csv',True,True)
 
 data, labels = read_data('Polygon Article Categorization Dataset.csv')
 
-print(predictor.test_bayes(data,labels))
+print("Bayes accuracy + precision:",predictor.test_bayes(data,labels))
 
-print(predictor.test_whitelist(data,labels))
+print("Whitelist accuracy + precision:", predictor.test_whitelist(data,labels))
