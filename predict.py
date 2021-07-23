9dee2934c0583cf1e98f2529c2831f22e13eb9b3
Soldann <guotl321@gmail.com>
2021-07-22 22:44:55 -0400

    fix csv used in predict

--- predict.py
+++ predict.py
@@ -5,7 +5,7 @@ from preprocess import *
 
 class Predict:
     def __init__(self,training_csv: str, filter_stop_words=True, stem_words=True) -> None:
-        self.word_to_category_probabilities, self.category_probabilities, self.default_probability = probabilities.get_probabilities('training.csv',filter_stop_words,stem_words)
+        self.word_to_category_probabilities, self.category_probabilities, self.default_probability = probabilities.get_probabilities(training_csv,filter_stop_words,stem_words)
         self.filter_stop_words = filter_stop_words
         self.stem_words = stem_words
         self.whitelist_dictionary = probabilities.create_whitelist_dictionary(self.word_to_category_probabilities)
